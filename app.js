cebcd1a4ec9a1bd624d3fd3237ead5796e5e4b67
Jan Soendermann <jan.soendermann@googlemail.com>
2014-05-03 23:54:45 +0100

    Set up routes

--- app.js
+++ app.js
@@ -5,26 +5,26 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var indexRouter = require('./routes/index');
+var splitRouter = require('./routes/split');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
-
+app.use('/', indexRouter);
+app.use('/split', splitRouter);
 
 
 module.exports = app;
