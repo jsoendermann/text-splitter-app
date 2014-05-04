b47d97e08b7850b30dea70490234dd440e1350c9
Jan Soendermann <jan.soendermann@googlemail.com>
2014-05-04 23:29:53 +0100

    Make index a redirect to zaoyin, remove lots of unneeded stuff

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -10,20 +8,14 @@ var app = express();
 var text_splitter = require('./text-splitter/text-splitter');
 text_splitter.initialize('./text-splitter/cedict-data.txt');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-
 app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.redirect("http://www.zaoyin.eu/text-splitter-api");
 });
 
 app.post('/split', function(req, res) {
