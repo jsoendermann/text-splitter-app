188fefb01f9fe86546312aa102b39e1f13a985ae
Jan Soendermann <jan.soendermann@googlemail.com>
2014-07-20 00:48:14 +0100

    Add endpoint to convert between simp/trad

--- app.js
+++ app.js
@@ -33,29 +33,33 @@ app.get('/', function(req, res) {
 });
 
 app.post('/split', function(req, res) {
-    var text = req.body.text;
-    var charset = req.body.charset;
+    var text, charset, output;
 
-    if (charset === "simp" || charset === "trad") {
-        var output = JSON.stringify(
+    text = req.body.text;
+    charset = req.body.charset;
+
+    if (charset === 'simp' || charset === 'trad') {
+        output = JSON.stringify(
             {
-                result:     "ok", 
+                result:     'ok', 
                 split_text: text_splitter.split_string(text, charset)
             }) + '\n';
 
         res.send(output);
     } else {
-        res.send(JSON.stringify({result: "error"}) + '\n');
+        res.send(JSON.stringify({result: 'error', reason: 'Invalid input'}) + '\n');
     }
 });
 
 app.post('/cedict', function(req, res) {
-    var word = req.body.word;
-    var charset = req.body.charset;
+    var word, charset, collection, query;
+
+    word = req.body.word;
+    charset = req.body.charset;
 
     if (typeof word !== 'undefined' && (charset === 'simp' || charset === 'trad')) {
-        var collection = req.db.get('cedict');
-        var query = {};
+        collection = req.db.get('cedict');
+        query = {};
         query[charset] = word;
 
         collection.find(query).on('success', function (doc) {
@@ -64,10 +68,54 @@ app.post('/cedict', function(req, res) {
 
             res.send(JSON.stringify({result: 'ok', records: doc}) + '\n');
         }).on('failure', function() {
-            res.send(JSON.stringify({result: 'error'}) + '\n');
+            res.send(JSON.stringify({result: 'error', reason: 'DB error'}) + '\n');
         });
     } else {
-        res.send(JSON.stringify({result: 'error'}) + '\n');
+        res.send(JSON.stringify({result: 'error', reason: 'Invalid input'}) + '\n');
+    }
+});
+
+app.post('/charset-conversion', function(req, res) {
+    var text, charset, target_charset, split_text, collection, converted_array, convert_word, current_word, query;
+    
+    text = req.body.text;
+    charset = req.body.charset;
+    
+    if (typeof text !== 'undefined' && (charset === 'simp' || charset === 'trad')) {
+        if (charset === 'simp') {
+            target_charset = 'trad';
+        } else {
+            target_charset = 'simp';
+        }
+
+        split_text = text_splitter.split_string(text, charset);
+        collection = req.db.get('cedict');
+        converted_array = [];
+
+        convert_word = function (index) {
+            if (index >= split_text.length) {
+                res.send(JSON.stringify({result: 'ok', 
+                                         converted_text: converted_array.join('')}) + '\n');
+            } else {
+                current_word = split_text[index];
+                query = {};
+                query[charset] = current_word;
+
+                collection.find(query).on('success', function (doc) {
+                    if (doc.length > 0) {
+                        converted_array[index] = doc[0][target_charset];
+                    } else {
+                        converted_array[index] = current_word;
+                    }
+                    convert_word(index + 1);
+                }).on('failure', function() {
+                    res.send(JSON.stringify({result: 'error', reason: 'DB error'}) + '\n');
+                });
+            }
+        };
+        convert_word(0);
+    } else {
+        res.send(JSON.stringify({result: 'error', reason: 'Invalid input'}) + '\n');
     }
 });
 
